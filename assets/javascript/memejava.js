48f6906d45b49ccc07076c820e3330376c466e85
Dima <dima757@hotmail.com>
2018-10-03 21:09:44 -0500

    dmitcmt

---
+++ assets/javascript/memejava.js
@@ -0,0 +1,38 @@
+// my api 38665cbc-8b63-4405-b13f-d83ec34704c9
+
+//URL for query for Trending Meme Widget
+var queryURL = "https://api.imgflip.com/get_memes"
+
+
+//vars to capture JSON returns
+
+var titleDisplay1 = $("<div>");
+var imgDisplay1 = $("<img>");
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+
+    // After the call is complete
+    .then(function(response) {
+        console.log(response);  //log out endpoint JSON object
+
+        //get data
+        var title = response.data.memes[0].name;
+        var url = response.data.memes[0].url;
+        
+        var explanation = response.explanation;
+        console.log(title+"Dmitriy");
+
+
+        titleDisplay1.text(title);
+        titleDisplay1.attr("class","text-center");
+        imgDisplay1.attr("src",url);
+        imgDisplay1.attr("class","img-fluid m-1");
+        imgDisplay1.attr("alt",explanation);
+
+        //append image of day content to widget in index.html
+       $("#meme-widget").append(imgDisplay1, titleDisplay1);
+
+});
